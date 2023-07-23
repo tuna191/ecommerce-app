0ebfca88f869ea261d9dfa9ca9960cd9650bc81e
DongTuna <dongtuna@MacBook-Pro-DongTuna.local>
2023-07-23 08:56:18 +0700

    orders page

---
+++ app/(dashboard)/[storeId]/(routes)/orders/components/client.tsx
@@ -0,0 +1,25 @@
+"use client"
+
+import Heading from "@/components/ui/Heading"
+import { Separator } from "@/components/ui/separator"
+import {OrderColumn, columns} from './columns'
+import { DataTable } from "@/components/ui/data-table"
+
+
+interface OrdersClientProps{
+    data: OrderColumn[]
+}
+export const OrdersClient = ({data} :OrdersClientProps) =>{
+
+    return (
+        <>
+            <Heading
+             title={`Orders(${data.length})`}
+             description="Manage orders for your store "
+            />
+            <Separator/>
+            <DataTable searchKey="products" columns={columns} data={data}/>
+            
+        </>
+    )
+}
